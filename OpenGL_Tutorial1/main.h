4aab69cbac7b3b1d46a70c620ad50d075de07c1c
callumnewlands <callumnewlands@googlemail.com>
2018-08-19 23:08:10 +0100

    Removed unecessary includes

--- OpenGL_Tutorial1/main.h
+++ OpenGL_Tutorial1/main.h
@@ -3,14 +3,9 @@
 // OpenGl Library Files
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
-#include "glm-0.9.9.0\glm-0.9.9.0\glm\glm.hpp"
-#include "glm-0.9.9.0\glm-0.9.9.0\glm\gtc\matrix_transform.hpp"
-#include "glm-0.9.9.0\glm-0.9.9.0\glm\gtc\type_ptr.hpp"
 
 // Standard Libraries
 #include <iostream>
-#include <math.h>
-#include <vector>
 
 void onWindowResize(GLFWwindow* window, int width, int height);
 
